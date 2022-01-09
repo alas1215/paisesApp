e5d2e471f6f9c03257a09a874aaa48aadf91270a
alas1215 <auribel1215@gmail.com>
2022-01-09 20:22:57 +0100

    partial commit

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class VerPaisComponent implements OnInit {
-
-  constructor() { }
+  pais!: Country;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private paisService: PaisService
+  ) {}
 
   ngOnInit(): void {
-  }
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)), tap())
+      .subscribe(pais => this.pais = pais);
 
+    //opcion 2
+    // this.activatedRoute.params.subscribe(({ id }) => {
+    //   this.paisService.getPaisPorAlpha(id).subscribe((pais) => {});
+  }
 }
